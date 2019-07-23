cb3f69dfd05cb504add623d15286520b00b927aa
AugustoMarcelo <mrclgst10@gmail.com>
2019-07-23 13:26:35 -0300

    Styling Sig in page and components

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,12 +1,72 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useRef, useState } from 'react';
+import { Image } from 'react-native';
+import PropTypes from 'prop-types';
+
+import logo from '~/assets/logo.png';
 
 import Background from '~/components/Background';
 
-export default function SignIn() {
+import {
+    Container,
+    Form,
+    FormInput,
+    SubmitButton,
+    SignLink,
+    SignLinkText,
+} from './styles';
+
+export default function SignIn({ navigation }) {
+    const passwordRef = useRef();
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    function handleSubmit() {}
+
     return (
         <Background>
-            <Text>Sign In</Text>
+            <Container>
+                <Image source={logo} />
+
+                <Form>
+                    <FormInput
+                        icon="mail-outline"
+                        keyboardType="email-address"
+                        autoCorrect={false}
+                        autoCapitalize="none"
+                        placeholder="E-mail"
+                        returnKeyType="next"
+                        onSubmitEditing={() => passwordRef.current.focus()}
+                        value={email}
+                        onChangeText={setEmail}
+                    />
+
+                    <FormInput
+                        icon="lock-outline"
+                        secureTextEntry
+                        placeholder="Password"
+                        autoCapitalize="none"
+                        ref={passwordRef}
+                        returnKeyType="send"
+                        onSubmitEditing={handleSubmit}
+                        value={password}
+                        onChangeText={setPassword}
+                    />
+
+                    <SubmitButton loading={false} onPress={handleSubmit}>
+                        Sign In
+                    </SubmitButton>
+                </Form>
+                <SignLink onPress={() => navigation.navigate('SignUp')}>
+                    <SignLinkText>Create free account</SignLinkText>
+                </SignLink>
+            </Container>
         </Background>
     );
 }
+
+SignIn.propTypes = {
+    navigation: PropTypes.shape({
+        navigate: PropTypes.func.isRequired,
+    }).isRequired,
+};
